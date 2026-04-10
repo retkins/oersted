fix meshing units

examples/benchmark.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from time import perf_counter
 
 
-def main(nbenches: int = 1, theta: float = 0.5, mesh_size_max: float = 33.0, mesh_size_min: float = 33.0):
+def main(nbenches: int = 1, theta: float = 0.5, mesh_size_max: float = 0.033, mesh_size_min: float = 0.033):
     mesh_sizes = np.linspace(mesh_size_max, mesh_size_min, nbenches)
     direct_times = []
     direct_interactions = []

examples/benchmark_error.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
 
 
-def main(nthetas: int = 1, size: float = 15.0, theta_min: float = 0.5, theta_max: float = 0.5):
+def main(nthetas: int = 1, size: float = 0.015, theta_min: float = 0.5, theta_max: float = 0.5):
     theta_vals = np.linspace(theta_min, theta_max, nthetas)
     errs = np.zeros(nthetas)
     mesh, jdensity = make_helmholtz("tests/data/ring.stp", size)